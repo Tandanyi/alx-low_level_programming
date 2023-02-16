d703158b66e75457f5798b4af48c8b8093688faa
Tandanyi <tattyndanyi@gmail.com>
2023-02-16 15:49:47 -0800

    trial 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+ /*declare variable */
+	 
+	const char string = "Programming is like building a multilingual puzzle";
+	
+/*using puts as output*/
+		puts(string);
+			return 0;
+}

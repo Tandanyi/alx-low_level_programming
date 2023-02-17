b7e31494681f124d77840afab386ab456a8a7573
Tandanyi <tattyndanyi@gmail.com>
2023-02-17 13:58:52 -0800

    fixes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,12 @@
 int main(void)
 {
 	int n;
-	int mod = n % 10;
 	char *s = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	int mod = n % 10;
+
 	/* your code goes there */
 	if (mod > 5 && !0)
 	{

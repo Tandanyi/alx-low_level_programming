0cd284ac58eed54941559d93d69881e8bbc4fc3b
Tandanyi <tattyndanyi@gmail.com>
2023-02-17 12:32:54 -0800

    fixes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/*if var is positive print positive else negative*/
 	if (n >= 0)
-		{printf("%d is positive/n", n);
+		{printf("%d is positive\n", n);
 		}
 		else
-		{printf("%d is negative/n", n);
+		{printf("%d is negative\n", n);
 		}
 	return (0);
 }

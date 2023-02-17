09baa38452cc877ff701c8a05c3263d32013eb52
Tandanyi <tattyndanyi@gmail.com>
2023-02-16 17:56:49 -0800

    finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int ll_int;
 	float float_type;
 /*get size*/
-	printf("Size of a char:", sizeof(char_type),"\n",
-		"Size of an int:", sizeof(int_type),"\n",
-		"Size of a long int:", sizeof(l_int),"\n",
-		"Size of a long long int:", sizeof(ll_int),"\n",
-		"Size of a float: ",sizeof(float_type),"\n");
+	printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int_type));
+	printf(	"Size of a long int: %zu byte(s)\n", sizeof(l_int));
+	printf(	"Size of a long long int:(%zu byte(s)\n", sizeof(ll_int));
+	printf(	"Size of a float: %zu byte(s)\n", sizeof(float_type));
 		return (0);
 }

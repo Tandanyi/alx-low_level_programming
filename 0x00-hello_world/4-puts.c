bcd4a69fe221c86450c21a2a8daec51cce93ba9a
Tandanyi <tattyndanyi@gmail.com>
2023-02-16 16:09:49 -0800

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 
-int main(){
- /*declare variable */
-	 
-	const char* string = "\"Programming is like building a multilingual puzzle";
-	
+int main(void)
+	{
+ /*declare variable */ 
+	const char *string = "\"Programming is like building a multilingual puzzle";
 /*using puts as output*/
 		puts(string);
-			return 0;
+			return (0);
 }

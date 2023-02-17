5035b020ac523b11163a0867b13bcf526596186e
Tandanyi <tattyndanyi@gmail.com>
2023-02-16 18:01:10 -0800

    Bbetty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 /*get size*/
 	printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int_type));
-	printf(	"Size of a long int: %zu byte(s)\n", sizeof(l_int));
-	printf(	"Size of a long long int:(%zu byte(s)\n", sizeof(ll_int));
-	printf(	"Size of a float: %zu byte(s)\n", sizeof(float_type));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(l_int));
+	printf("Size of a long long int: (%zu byte(s)\n", sizeof(ll_int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
 		return (0);
 }

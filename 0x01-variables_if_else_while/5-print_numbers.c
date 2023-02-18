e888dd1ceb96e287685ee5e438b8e69387442784
Tandanyi <tattyndanyi@gmail.com>
2023-02-17 16:24:16 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -prints numbers
+ * Description: prints numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}

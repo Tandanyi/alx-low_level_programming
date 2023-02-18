afff134282ef8dbd9b13e41e273db0586f65213f
Tandanyi <tattyndanyi@gmail.com>
2023-02-17 18:48:16 -0800

    betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else if (n % 10 == 0)
         {
-		printf("%s %d is %d and is 0\n", s, n, n % 10)
+		printf("%s %d is %d and is 0\n", s, n, n % 10);
 	}
 	else if (n % 10 < 6 && !0)
 	{

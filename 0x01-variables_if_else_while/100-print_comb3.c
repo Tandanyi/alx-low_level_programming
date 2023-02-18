e61c9a4273964725deba47e25600d4f30ea65555
Tandanyi <tattyndanyi@gmail.com>
2023-02-17 18:42:45 -0800

    1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main- main function
+ * Description: prints combination of digits
+ * Return: 0
+ */
+int main(void)
+{
+	int n,n2;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (n2 = '1'; n2 <= '9'; n2++)
+		{
+			if (n > n2)
+			{
+				putchar(n);
+				putchar(n2);
+				if (n !='9' || n2 !='10')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
+}

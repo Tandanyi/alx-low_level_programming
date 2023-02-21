1ddf81cd3568aeed07cdf90a87ec6c89d2c2e27f
Tandanyi <tattyndanyi@gmail.com>
2023-02-21 01:58:30 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ * Description :declares a string
+ * prints "_putchar"
+ * Return: 0
+ */
+int main(void)
+{
+	char *str = "_putchar";
+
+	printf("%s\n", str);
+	return (0);
+}

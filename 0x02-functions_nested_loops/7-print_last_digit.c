1a75731da1dab09eeb0ab1931786f27655ceaf57
Tandanyi <tattyndanyi@gmail.com>
2023-02-21 13:14:01 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit- prints the last digit
+ * @n: input value
+ *
+ * Description: prints the last digit of a number
+ * Return: new n
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}

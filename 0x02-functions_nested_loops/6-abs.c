d520e8c34a0556a305430d842c6c4f22eada8218
Tandanyi <tattyndanyi@gmail.com>
2023-02-21 12:26:06 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs -gets absolute value
+ * @int: input value
+ *
+ * Description: changes a number to its abolute value
+ * Return: int
+ */
+
+int _abs(int)
+{
+	if (int >= 0)
+	{
+		return (int);
+	}
+	else
+	{
+		int + (-2 * int);
+		return (int);
+	}
+}

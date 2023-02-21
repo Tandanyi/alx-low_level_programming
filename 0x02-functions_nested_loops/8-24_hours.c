617060ee55fe94d16b4660166216722ff73fefa5
Tandanyi <tattyndanyi@gmail.com>
2023-02-21 15:14:23 -0800

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,14 +6,14 @@
  */
 
 void jack_bauer(void)
-{	int h, m;
+{	int h, m, ;
 
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar('0' + h);
-			_putchar('0' +  m);
+			_putchar((h) + '0');
+			_putchar((m) + '0');
 			_putchar('\n')
 		}
 	}

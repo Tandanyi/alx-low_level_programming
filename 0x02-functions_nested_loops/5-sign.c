e769465d4ebb65929c7e00ee4a219e531167ac84
Tandanyi <tattyndanyi@gmail.com>
2023-02-21 11:55:16 -0800

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,25 @@
 
 /**
  * print_sign - determines the sign of a number
- * @n - number being determined
+ * @n: number being determined
+ *
  * Description: determines whether a number is positive or negative
- * Return:0
+ * Return:0 -when nuber is 0
+ * 1 -when number is positive
+ * -1 -when number is negative
  */
 
-
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{	return (0);
+	}
+}

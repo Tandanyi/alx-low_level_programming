3868bafacbefd0ab61717dfefe16be5c23ac2468
Tandanyi <tattyndanyi@gmail.com>
2023-02-23 14:32:07 -0800

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n- input length
+ *
+ * Description: prints a line according to n value
+ * if n is 0 print only new line
+ * Return- nothing
+ */
+
+void print_line(int n)
+{
+	int m;
+	
+	for(m = 1; m >= n; m++)
+	{
+		if( n == 0)
+		{
+			break;
+		}
+		_putchar('-');
+	}
+	_putchar('\n');
+}

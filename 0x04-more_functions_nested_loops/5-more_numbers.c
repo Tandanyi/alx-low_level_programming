eef9cdc1bac8e89b8090da3ba89d93ab7e5e5b8c
Tandanyi <tattyndanyi@gmail.com>
2023-02-23 13:41:52 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_more_numbers - prints 0 - 14
+ * Description- prints 0 -14 10 times
+ * Return: nothing
+ */
+
+void print_more_numbers(void)
+{
+	int i;
+	int j;
+
+	for(j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}

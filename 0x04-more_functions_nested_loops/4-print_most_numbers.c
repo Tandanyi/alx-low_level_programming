a4e92db2db6bc3da7d1fd18cf717203007be99f4
Tandanyi <tattyndanyi@gmail.com>
2023-02-23 13:26:03 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ *
+ * Return - nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9)
+	{
+		if (i == 2 && i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}

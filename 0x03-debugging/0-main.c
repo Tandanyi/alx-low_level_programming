6d7ccc0609e4aaac1cdd8af8ab6ea418ccf60ef4
Tandanyi <tattyndanyi@gmail.com>
2023-02-23 03:31:13 -0800

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - tests main.c
+ * Description: tests whether the function in main.c gives a correct output
+ * Return: 0 when correct
+ * 1 when wrong
+ */
+
+int main(void)
+{
+	char r;
+	int i = 0;
+
+	r = positive_or_negative(i);
+	if (r == '0 is zero')
+	{
+		return (0);
+	}
+	else if (r != '0 is zero')
+	{
+		return (1);
+	}
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+this is the third challenge of alx low level programming
+it tests debugging functions.

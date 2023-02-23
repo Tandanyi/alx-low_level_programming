4f362cc0506fa3c19df1ff38e5456a14220e6190
Tandanyi <tattyndanyi@gmail.com>
2023-02-23 13:53:57 -0800

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,12 @@
 
 void more_numbers(void)
 {
-	int i;
+	char i;
 	int j;
 
 	for(j = 0; j <= 10; j++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = '0'; i <= '14'; i++)
 		{
 			_putchar(i + '0');
 		}

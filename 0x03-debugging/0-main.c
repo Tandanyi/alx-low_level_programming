46ec6e42c7f4301bbc34c86d102137dcbe7efbcc
Tandanyi <tattyndanyi@gmail.com>
2023-02-23 03:51:42 -0800

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,21 +4,12 @@
  * main - tests main.c
  * Description: tests whether the function in main.c gives a correct output
  * Return: 0 when correct
- * 1 when wrong
  */
 
 int main(void)
 {
-	int r;
 	int i = 0;
 
-	r = positive_or_negative(i);
-	if (r == "0 is zero")
-	{
-		return (0);
-	}
-	else if (r != "0 is zero")
-	{
-		return (1);
-	}
+	positive_or_negative(i);
+	return (0);
 }

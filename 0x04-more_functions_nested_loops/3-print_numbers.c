a33343a34323da4adb24e81f24c33e632b051e78
Tandanyi <tattyndanyi@gmail.com>
2023-02-23 12:55:32 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 - 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0', i <= '9', i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

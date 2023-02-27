208b0b9b4115d9509c4c770a409e52c24dd7cb76
Tandanyi <tattyndanyi@gmail.com>
2023-02-27 15:49:32 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * puts_half - prints half of string
+ * @str- string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int l;
+
+	l = _strlen-1;
+
+	for ( l = l/2; str[l] != '\0'; l++)
+	{
+		_putchar(str[l]);
+	}
+	_putchar('\n');
+
+
+}
+
+/**
+ * _strlen - looks for the size of a string
+ * @s: string
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}

b0002ad6f69bf5041ad9bd747de60a958b9a59ff
Tandanyi <tattyndanyi@gmail.com>
2023-02-27 12:51:20 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - looks for the size of a string
+ * @s: string
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	while (l = 0; s[l] != '0') 
+	{
+		l++;
+	}
+	return(l);
+}

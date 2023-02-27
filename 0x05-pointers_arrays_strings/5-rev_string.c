ca121b8dee58ad7a9c3a63e1480b9203ac9ae226
Tandanyi <tattyndanyi@gmail.com>
2023-02-27 14:58:47 -0800

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,11 @@
 
 void rev_string(char *s)
 {
-	int i,j;
+	int i, j;
 	char k;
 
-	for (i = 0, j = _strlen(s) - 1; i<j; i++, j--) {
+	for (i = 0, j = _strlen(s) - 1; i < j; i++, j--)
+	{
 		k = s[i];
 		s[i] = s[j];
 		s[j] = k;

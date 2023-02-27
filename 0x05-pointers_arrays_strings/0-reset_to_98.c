9fe7a657a1fe6ad2dbb79e6953a748d19b899c02
Tandanyi <tattyndanyi@gmail.com>
2023-02-27 11:40:57 -0800

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - assigns 98 to a variable
+ * @n: pointer declaration
+ * Description: assigns 98 t a variable n using a pointer
+ * Return : nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is the 5th project on alx-low level programming
+it tests the understanding of pointers and arrays and memory layout

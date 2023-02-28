629994ef5dd7ccc3161230ff4d564a8fd89be4de
Tandanyi <tattyndanyi@gmail.com>
2023-02-27 17:13:07 -0800

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == n - 1)
-			printf("%d\n", a[i]);
-		else
+		if (i != n - 1)
 			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
 	}
 }

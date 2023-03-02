0712e1d18c56fdf6bd5233634cc21743f1eab8b2
Tandanyi <tattyndanyi@gmail.com>
2023-03-02 13:43:33 -0800

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,16 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	dest[i] = '\0';
+	i = 0;
+	while(dest[i] != '\0')
+	{
+		i++;
+	}
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
 	dest[i] = '\0';
+	return (dest);
 }

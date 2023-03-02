a5df66354510b6ff6aecff7a3b9cf80804a5c290
Tandanyi <tattyndanyi@gmail.com>
2023-03-02 14:25:22 -0800

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the contents of an array
+ * @a: array
+ * @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		j = a[i];
+		a[i] = a[n - i - 1];
+		j = a[n - i - 1];
+	}
+}

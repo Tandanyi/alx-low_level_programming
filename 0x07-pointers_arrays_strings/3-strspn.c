70e92ca1d5ee0ddc3f628d62399dd7fbd73091fc
Tandanyi <tattyndanyi@gmail.com>
2023-03-06 14:41:57 -0800

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * strpn - gets length of substring
+ * @s : source
+ * @accept : a substring of characters
+ *
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int a = 0;
+	int b;
+
+	while (*s)
+	{
+		i = 0;
+		b = a;
+		while (*(accept + i) != '\0')
+		{
+			if (*(accept + i) == *s)
+				a++;
+			i++;
+		}
+		if (b == a)
+			break;
+		s++;
+	}
+	return (a);
+}

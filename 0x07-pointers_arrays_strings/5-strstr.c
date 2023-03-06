901c959a2e095de23fcf4eb8d4755cbe12aa03c8
Tandanyi <tattyndanyi@gmail.com>
2023-03-06 15:16:57 -0800

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - finds a substring
+ * @haystack - source
+ * @needle - occurence of string in source
+ *
+ * Returns: pointer haystack if string is found
+ * 	null if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[index])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+
+			} while (haystack[i] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}

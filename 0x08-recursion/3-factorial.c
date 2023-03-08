228b86487997043d58441c76ed3324ec11c3d6c1
Tandanyi <tattyndanyi@gmail.com>
2023-03-08 15:46:45 +0000

    negative inclusion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,11 +5,14 @@
  * @n : number input
  *
  * Return: factorial n
+ * -1 when n is negative
  */
 
 int factorial(int n)
 {
-	if( n == 0)
-		return(1);
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
 	return (n * factorial(n - 1));
 }

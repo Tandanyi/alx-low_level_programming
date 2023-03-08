8727b89b5353eb2f7cda2d670631cc3f8f271946
Tandanyi <tattyndanyi@gmail.com>
2023-03-08 15:00:48 +0000

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints length of string
+ * @s : string literal
+ *
+ * return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (!*s)
+		return (1);
+	return (i + _strlen_recursion(s + 1););
+}

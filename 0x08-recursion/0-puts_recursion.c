47fa63c68fe633ac01b4f88864bed668e8b2bd98
Tandanyi <tattyndanyi@gmail.com>
2023-03-08 13:12:41 +0000

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,8 +9,11 @@
 void _puts_recursion( char *s)
 {
 	_putchar(*s);
-	if (*s != '\0')
-		_puts_recursion(*s++);
-	else
+	if (*s == '\0')
+	{
 		_putchar('\n');
+		return;
+	}
+	*s++;
+	_puts_recursion(s);
 }

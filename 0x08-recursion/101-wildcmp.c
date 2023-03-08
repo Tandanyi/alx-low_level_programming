12d4a96089aa655420467c056bffc79520986ac6
Tandanyi <tattyndanyi@gmail.com>
2023-03-08 23:03:32 +0000

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,20 +5,20 @@
  * @s1 : first string
  * @s2 : 2nd string
  *
- * Return : 1 if identcal
+ * Return: 1 if identcal
  * 0 if not
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	if(*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
-	if(!*s1 && !*s2)
+	if (!*s1 && !*s2)
 		return (1);
 	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 +1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 				return (0);
 
 }

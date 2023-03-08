9e5096026aad79f70052690b28a17957591bef3d
Tandanyi <tattyndanyi@gmail.com>
2023-03-08 15:33:08 +0000

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,12 +4,12 @@
  * _strlen_recursion - prints length of string
  * @s : string literal
  *
- * return: length of string
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
 {
 	if (!*s)
-		return (1);
-	return (_strlen_recursion(s) + _strlen_recursion(s + 1));
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }

f45974e906d774ae10476d495ce4d337ac18a4de
Tandanyi <tattyndanyi@gmail.com>
2023-03-10 15:40:06 -0800

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints program name 
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
+

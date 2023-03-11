41aef2c1439b2554dfa3701b5e848f6c7ab8f19c
Tandanyi <tattyndanyi@gmail.com>
2023-03-10 16:16:28 -0800

    1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-  * main - Prints program name 
+  * main - Prints program name
   * @argc: argument count
   * @argv: argument vector
   *
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
 
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ * argc : argument count
+ * argv : argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

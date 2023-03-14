9bac2f18b0963f40808c9e1a7171abb36d4465eb
Tandanyi <tattyndanyi@gmail.com>
2023-03-14 15:06:32 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,21 +17,31 @@ char *str_concat(char *s1, char *s2)
 	int n;
 	int size;
 
-	size = strlen(s1) + strlen(s2);
-	newstr = (char * ) malloc(sizeof(char) * size);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	newstr = malloc(sizeof(char) * (i + j + 1));
+
 	if (newstr == NULL)
-		return (NULL);
-	else
 	{
-		while (n = 0, s1[n] != '\0')
-		{
-			newstr[n] = s1[n];
-			n++;
-		}
-		for (; n < size; n++)
-			newstr[n] = s2[n];
-		}
-	return (newstr);
+		free(newstr);
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+		newstr[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		newstr[k] = s2[j];eturn (newstr);
 
 }
 

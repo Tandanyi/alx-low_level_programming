07dd568f0ef1187240f1bc0c7b15ceff62a411f4
Tandanyi <tattyndanyi@gmail.com>
2023-03-14 18:08:29 +0000

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		str = malloc(sizeof(char) * size);
+		str = char * malloc(sizeof(char) * size);
 		for(n = 0; n <= size; n++)
 		{
 			str[n] = c;

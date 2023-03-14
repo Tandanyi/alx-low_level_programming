3e880173bdc8e19ee5b665555f805bc385c335ba
Tandanyi <tattyndanyi@gmail.com>
2023-03-14 19:18:42 +0000

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,10 @@ char *create_array(unsigned int size, char c)
 		return ('\0');
 	}
 	str = malloc(sizeof(char) * size);
+	if (str == '\0')
+	{
+		return ('\0');
+	}
 	for (n = 0; n <= size; n++)
 	{
 		str[n] = c;
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a copy of a string to a new mem space
+ * @str: string
+ *
+ * return: null if str is null
+ * 	: str

cf7bc1186827b0a5ae08dc0b2a5649936f0e5a0e
Tandanyi <tattyndanyi@gmail.com>
2023-03-20 16:26:19 -0700

    0

---
+++ 0x0E-structures_typendef/README.md
@@ -0,0 +1,2 @@
+Structures, typedef
+this project shows my understanding of C structures and typedef
---
+++ 0x0E-structures_typendef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog
+ * @name : name of dog
+ * @age : age of dog
+ * @owner : the owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif

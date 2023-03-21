670c90ef959cb5e2f90af0ea65a513a91b167217
Tandanyi <tattyndanyi@gmail.com>
2023-03-20 17:46:57 -0700

    fixing errors

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(dog));
+		d = malloc(sizeof(dog_t));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,7 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);

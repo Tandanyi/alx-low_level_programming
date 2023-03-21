fbe44fca8525bc9cc9689d5ad1433a9f8a731b2e
Tandanyi <tattyndanyi@gmail.com>
2023-03-20 17:27:22 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+		free(d->name);
+		free(d->owner);
+		free(d);	
+}

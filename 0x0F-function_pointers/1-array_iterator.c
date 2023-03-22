b5aa02b46d4877c892a89c5decec31220addb1ea
Tandanyi <tattyndanyi@gmail.com>
2023-03-22 15:49:27 -0700

    changing env

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - performs a function for every var
+ * @array : array if intergers
+ * @size : size of array
+ * @action : function pointer
+ */
+
+void *array_iterator(int *array, size_t size, void(*action)(int))
+{
+	long unsigned int i;
+	if (array == NULL && size == 0)
+		return;
+	for (i = 0; i <= size; i++)
+		action(array[i]);
+}

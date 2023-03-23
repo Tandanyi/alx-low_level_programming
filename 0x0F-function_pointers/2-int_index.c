07a42d2d3f9cc2302f0726c784ac1ed82a46b257
Tandanyi <tattyndanyi@gmail.com>
2023-03-23 00:02:03 +0000

    Betty corrections

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,7 @@
  * @size : sze of array
  * @cmp : function pointer
  *
- * Return : int_index returns the index of the first element when success
+ * Return: int_index returns the index of the first element when success
  * If no element matches, return -1
  * If size <= 0, return -1
  */
@@ -17,13 +17,13 @@ int int_index(int *array, int size, int(*cmp)(int))
 	int i;
 
 	if (!array || !cmp)
-		return -1;
+		return (-1);
 	if (size <= 0)
-		return -1;
+		return (-1);
 	for (i = 0; i <= size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return i;
+			return (i);
 	}
-	return -1;
+	return (-1);
 }

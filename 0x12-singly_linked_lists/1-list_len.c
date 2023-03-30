064a228071e61cf1529e48ff0f28504f4e7cb425
Tandanyi <tattyndanyi@gmail.com>
2023-03-30 11:49:25 -0700

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * list_len - counts contents of a linked list
+ * @h : list
+ *
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t l = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		l++;
+	}
+	return (l);
+
+}

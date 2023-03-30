9d18ef8489372e154d7534906e08aee77a564e2b
Tandanyi <tattyndanyi@gmail.com>
2023-03-30 12:42:50 -0700

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add node- adds a list tothe beginning of list
+ * @head - start of list
+ * @str - new list
+ *
+ * Return: null if failed
+ * new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	char *dup;
+	int len;
+
+	for (len = 0; str[len];)
+	{
+		len++;
+	}
+
+	temp = malloc(sizeof(list_t));
+	if (temp = NULL)
+		return (NULL);
+
+	dup = strdup(str);
+
+	if (dup == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	
+	temp->str = dup;
+	temp->next = *head;
+	temp->len = len + 1;
+	
+	*head = temp;
+
+	return(temp);
+}

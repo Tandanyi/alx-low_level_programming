2b72803acf957e8a348cd0b263277c37abeb1e43
Tandanyi <tattyndanyi@gmail.com>
2023-03-30 10:54:00 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints out  the contents of a linked list
+ * @h : list
+ *
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t len = 0;
+	
+	if(h == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	else
+	{
+		while (h != NULL)
+		{
+			len++;
+			printf("%s \n", h->str);
+			h = h -> next;
+		}
+	}
+	return(len);
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+Singly_linked_list
+This shows my understanding of simple linked lists

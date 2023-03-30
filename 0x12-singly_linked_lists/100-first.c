4bd038f48b030823d300025015aff97f3044e7d0
Tandanyi <tattyndanyi@gmail.com>
2023-03-30 14:56:07 -0700

    *betty corrections

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void __attribute__ ((constructor)) premain(void);
 
 void __attribute__ ((constructor)) premain(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-		    I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	 	"I bore my house upon my back!\n");
 }

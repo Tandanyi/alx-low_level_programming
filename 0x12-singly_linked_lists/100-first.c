262e3ca0c09ad5421e6970f324a526351b0cd50f
Tandanyi <tattyndanyi@gmail.com>
2023-03-30 14:52:21 -0700

    betty corrections

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+void __attribute__ ((constructor)) premain(void);
 /**
  * premain - executes code before main
  */
 
-void __attribute__ ((constructor)) premain()
+void __attribute__ ((constructor)) premain(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+		    I bore my house upon my back!\n");
 }

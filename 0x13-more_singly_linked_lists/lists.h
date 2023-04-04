a50f23f8458368833f644e1b2c81eac5061bb922
Tandanyi <tattyndanyi@gmail.com>
2023-04-04 23:06:21 +0000

    lists.h betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,21 +1,20 @@
+#ifndef LIST_H
+#define LIST_H
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
-#ifndef LIST_H
-#define LIST_H
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  * @visited: flag to indicate node s vsited when printing
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
-    int visited;
+	int n;
+	struct listint_s *next;
+	int visited;
 } listint_t;
 
 size_t print_listint(const listint_t *h);

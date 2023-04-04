c2b3c067da13169856396b34b76560de74d032c4
Tandanyi <tattyndanyi@gmail.com>
2023-04-04 20:16:29 +0000

    6-correctons

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,21 +2,21 @@
 
 /**
  * pop_listint - frees a list
- * @head- pointer to the start of the list
+ * @head: pointer to the start of the list
  *
  * Return: head nodes data
  */
 
 int pop_listint(listint_t **head)
 {
-        listint_t *temp;
+	listint_t *temp;
 	int data = (*head)->n;
 
 	if (head == NULL || *head == NULL)
 		return 0;
 
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-		return (data);
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (data);
 }

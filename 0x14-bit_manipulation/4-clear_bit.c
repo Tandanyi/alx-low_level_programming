4cb49e8e198c5f0225e8133d258c897d2693e715
Tandanyi <tattyndanyi@gmail.com>
2023-04-10 16:07:49 +0000

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
- * clear_bit - sets value to 0
+ * clear_bit - clears value of a bit index to 1
+ * @n : A pointer to the bit
+ * @index : index given
+ *
+ * Return: -1 if an error occurs
+ * 1 if successful
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n &= ~(1 << index);
+
+	return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ unsigned int power(unsigned int x, int y);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
